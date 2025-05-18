f8f302e2c67e269b8e83656c269893abe9cdbf35
DHA0015 <DHA0015@suzannecoryhs.vic.edu.au>
2025-05-18 23:05:37 +1000

    Updated welcome message

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 print("Hello there user!")
-print("Welcome to the project! Would you like a tour? y/n")
+print("Welcome to the project! We'll start with a welcome tour!")
+print("I hope you're excited, because this will be so much fun!")
